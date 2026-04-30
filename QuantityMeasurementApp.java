645e8dbecf2e698c5a34d384d9d5f0b8a891e845
Aditya12-Nahak <an4105@srmist.edu.in>
2026-04-30 20:12:16 +0530

    uc11

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -2,20 +2,20 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        // ===== LENGTH =====
-        Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
-        Quantity<LengthUnit> l2 = new Quantity<>(12.0, LengthUnit.INCHES);
+        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        Quantity<VolumeUnit> v3 = new Quantity<>(1.0, VolumeUnit.GALLON);
 
-        System.out.println(l1.equals(l2));
-        System.out.println(l1.convertTo(LengthUnit.INCHES));
-        System.out.println(l1.add(l2, LengthUnit.FEET));
+        // Equality
+        System.out.println(v1.equals(v2)); // true
 
-        // ===== WEIGHT =====
-        Quantity<WeightUnit> w1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
-        Quantity<WeightUnit> w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
+        // Conversion
+        System.out.println(v1.convertTo(VolumeUnit.MILLILITRE));
 
-        System.out.println(w1.equals(w2));
-        System.out.println(w1.convertTo(WeightUnit.GRAM));
-        System.out.println(w1.add(w2, WeightUnit.KILOGRAM));
+        // Addition
+        System.out.println(v1.add(v2)); // 2 litre
+
+        // Cross unit addition
+        System.out.println(v1.add(v3, VolumeUnit.MILLILITRE));
     }
 }
